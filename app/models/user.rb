512eecf1bc20607839935424097841dea987d443
Your Name <you@example.com>
2015-01-19 18:03:13 +0100

    initializing push

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
-class User < ActiveRecord::Base
+class User
+  include Mongoid::Document
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  field :name, type: String
+  field :email, type: String
+  field :encrypted_password,  type: String
+  field :current_sign_in_at,  type: Time
+  field :last_sign_in_at,  type: Time
+  field :current_sign_in_ip,  type: String
+  field :last_sign_in_ip,  type: String
+  field :sign_in_count,  type: Integer
+  field :remember_created_at, type: Time
 end
